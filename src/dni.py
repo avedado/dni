316cd7de9c10d8e7caac5ce4a59dac363458afa0
arianaborrajo <borrajoarias@gmail.com>
2025-01-23 13:28:47 +0100

    docs: Definir función init

---
+++ src/dni.py
@@ -0,0 +1,11 @@
+class dni:
+
+    def __init__(self, cadena = ""):
+        self.tabla = ["T","R","W","A","G","M","Y","F","P","D","X","B", \
+                      "N","J","Z","S","Q","V","H","L","C","K","E", ]
+        self.dni = cadena
+
+    def getDni(self):
+        return self.dni   
+    
+    
