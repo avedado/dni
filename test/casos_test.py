2934fea77ee5c80659763ee40b5fb74726e94f72
arianaborrajo <borrajoarias@gmail.com>
2025-01-24 13:52:10 +0100

    feat: Refactorizar archivo dni

--- test/casos_test.py
+++ test/casos_test.py
@@ -1,17 +1,27 @@
-casosTest = [  # casos OK
-        "78484464T",
-        "72376173A",
-        "01817200Q",
-        "95882054E",
-        "63587725Q",
-        "26861694V",
-        "21616083Q",
-        "26868974Y",
-        "40135330P",
-        "89044648X",
-        "80117501Z",
-        "34168723S",
-        "76857238R",
-        "66714505S",
-        "66499420A",
-    ]
+import pytest
+from src.tabla_asignacion import TablaAsignacion
+from src.dni import Dni
+
+def test_letra_valida(dni, esperado):
+    dni_obj = Dni(dni)
+    assert dni_obj.check_letra() == esperado
+
+
+def test_letra_invalida(dni, esperado):
+    dni_obj = Dni(dni)
+    assert dni_obj.check_letra() == esperado
+
+
+def test_calculo_letra(dni_sin_letra, letra_esperada):
+    dni_obj = Dni(dni_sin_letra + "X")  
+    assert dni_obj.obtener_letra() == letra_esperada
+
+
+def test_longitud_dni(dni, longitud_correcta):
+    dni_obj = Dni(dni)
+    assert dni_obj.check_longitud() == longitud_correcta
+
+
+def test_numero_valido(dni, es_valido):
+    dni_obj = Dni(dni)
+    assert dni_obj.check_numero() == es_valido
